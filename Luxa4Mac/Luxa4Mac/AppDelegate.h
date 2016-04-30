8549b27739738be66c821d691253608002ac2ec6
sdevore <sdevore@gmail.com>
2016-04-29 19:32:21 -0700

    Restart this mess

---
+++ Luxa4Mac/Luxa4Mac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Luxa4Mac
+//
+//  Created by Samuel DeVore on 4/29/16.
+//  Copyright © 2016 Samuel DeVore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Luxa4Mac/Luxa4Mac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Luxa4Mac
+//
+//  Created by Samuel DeVore on 4/29/16.
+//  Copyright © 2016 Samuel DeVore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
