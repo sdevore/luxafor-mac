5d34bcf1840b4f359ed0b5d5155ff0788e39d45d
sdevore <sdevore@gmail.com>
2016-05-01 21:21:29 -0700

    add the distributed version of the source from Luxafor

---
+++ Luxafor-OSX/LXUpdater.h
@@ -0,0 +1,15 @@
+//
+//  LXUpdater.h
+//  Luxafor-OSX
+//
+//  Created by Aigars Silavs on 19/04/15.
+//  Copyright (c) 2015 draugiem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LXUpdater : NSObject
+
++ (LXUpdater *)sharedInstance;
+
+@end
